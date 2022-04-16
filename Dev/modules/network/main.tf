eb097fd5ff1fd24d8139df5e3be7ac442d593be9
sjeganathan5 <103455152+sjeganathan5@users.noreply.github.com>
2022-04-15 22:32:59 -0400

    Add files via upload

---
+++ Dev/modules/network/main.tf
@@ -0,0 +1,104 @@
+# Step 1 - Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Data source for availability zones in us-east-1
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  default_tags = merge(
+    var.default_tags,
+    { "Env" = var.env }
+  )
+}
+
+# Create a new VPC 
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  tags = merge(
+    local.default_tags, {
+      Name = "${var.prefix}-${var.env}-vpc"
+    }
+  )
+}
+
+
+# Add provisioning of the public subnetin the default VPC
+resource "aws_subnet" "public_subnet" {
+  count             = length(var.public_cidr_blocks)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_cidr_blocks[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = merge(
+    local.default_tags, {
+      Name = "${var.prefix}-public-subnet-${count.index}"
+    }
+  )
+}
+
+# Add provisioning of the private subnet in the default VPC
+resource "aws_subnet" "private_subnet" {
+  count             = length(var.private_cidr_blocks)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_cidr_blocks[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = merge(
+    local.default_tags, {
+      Name = "${var.prefix}-private-subnet-${count.index}"
+    }
+  )
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${var.prefix}-igw"
+    }
+  )
+}
+
+
+# Route table to route add default gateway pointing to Internet Gateway (IGW)
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.prefix}-route-public-route_table"
+  }
+}
+
+# Route table to route add default gateway pointing to Internet Gateway (IGW)
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.prefix}-route-private-route_table"
+  }
+}
+
+# Associate subnets with the custom route table
+resource "aws_route_table_association" "public_route_table_association" {
+  count          = length(aws_subnet.public_subnet[*].id)
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+}
+
+# Associate subnets with the custom route table
+resource "aws_route_table_association" "private_route_table_association" {
+  count          = length(aws_subnet.private_subnet[*].id)
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+}
---
+++ Dev/network/main.tf
@@ -0,0 +1,13 @@
+
+# Module to deploy basic networking 
+module "vpc-dev" {
+  source = "../../Dev/modules/network"
+  #source              = "git@github.com:igeiman/aws_network.git"
+  env                = var.env
+  vpc_cidr           = var.vpc_cidr
+  public_cidr_blocks = var.public_cidr_blocks
+  prefix             = var.prefix
+  default_tags       = var.default_tags
+}
+
+#/ACS_Final_Project/modules/aws_network
